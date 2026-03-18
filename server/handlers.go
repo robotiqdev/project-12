0ef351ea21b5b4de251c3857b95cdc0e8c3bb87f
Sandbox <sandbox@factory>
2026-03-18 12:30:50 +0000

    review: code quality fixes for FEAT-1167

--- server/handlers.go
+++ server/handlers.go
@@ -7,8 +7,13 @@ import (
 
 // handleVersion responds with a JSON object containing the application version.
 func (s *Server) handleVersion(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
+	data, err := json.Marshal(map[string]string{
 		"version": s.config.Version,
 	})
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
