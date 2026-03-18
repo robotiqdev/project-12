de2e0cc331796d25e2cea3c18aab86acc5b53918
Sandbox <sandbox@factory>
2026-03-18 09:57:23 +0000

    feat: TASK-4633 - Add version variable documentation

--- version/version.go
+++ version/version.go
@@ -1,3 +1,6 @@
+// Package version provides the application version constant.
 package version
 
+// Version is the current release version of the server.
+// Update this value before each release.
 const Version = ""
